9595f225b3c5080169baa85b15486e3a795891b4
Kevin Ta <kvnta@yahoo.com>
2022-11-15 09:16:06 -0700

    Create models and their relationships to pass initial model setups

---
+++ app/models/passenger.rb
@@ -0,0 +1,7 @@
+class Passenger < ApplicationRecord 
+
+  has_many :flight_passengers
+  has_many :flights, through: :flight_passengers
+
+
+end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  describe 'relationships' do
-    it {should belong_to :airline}
-  end
+  it { should belong_to :airline }
+  it { should have_many :flight_passengers}
+  it { should have_many(:passengers).through(:flight_passengers)}
+
 end
